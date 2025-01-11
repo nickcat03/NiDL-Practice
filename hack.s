c9be739ebb2087169cb1aaf714fc6c7c4dc1beaa
nickcat03 <mariosnippos@gmail.com>
2025-01-11 17:15:05 -0500

    all levels unlocked

--- hack.s
+++ hack.s
@@ -32,10 +32,43 @@ _start = baseaddress
 @ HEX EDITS
 @@@@@@@@@@@@@@@@@@@@@@@
 
+@ Never lose lives
 patchat 0x08009A9A
     add r2, r2, #0
 
+@ Bonus doors (this also puts all stars in star hub room)
+patchat 0x08022590
+	cmp r0, #0xFF
+
+@ Always have levels unlocked
+@ Level doors
+patchat 0x080225E0
+    mov r0, #0x02
+
+patchat 0x0802781A
+	mov r0, #0x02
+
+@ bonus doors
+patchat 0x08027898
+    nop
+	nop
+
+patchat 0x08027946
+	mov r0, #0xFF
+
+patchat 0x08027964
+    mov r0, #0x02
+
+patchat 0x0802802C
+     mov r0, #0x02
+	 cmp r0, #0xFF
+	
+patchat 0x080280A2
+    mov r0, #0x02
+
+@ Leave this last
 patchat 0x08800000
+
 
 @0x249A84 blank spot
 @0x2FE000 blank
